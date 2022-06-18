fa974736ebeb43b6ed0fddb1bcf14e46127fc8a9
luis machaca <luismachaca@outlook.com.pe>
2022-06-17 21:46:37 -0500

    dashboard for mobile

--- time-tracking-dashboard-main/src/index.js
+++ time-tracking-dashboard-main/src/index.js
@@ -1,7 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const app = <h1>Hello World</h1>;
-const container = document.getElementById("app");
-
-ReactDOM.render(app, container);
+const navBar = document.getElementById("nav");
+const pages = document.querySelectorAll(".page");
+navBar.addEventListener("click", (e) => {
+  if (e.target.classList.contains("nav__link")) {
+    e.preventDefault();
+    if (e.target.href.includes("daily")) {
+      pages[0].style.zIndex = "3";
+      pages[1].style.zIndex = "2";
+      pages[2].style.zIndex = "1";
+    }
+    if (e.target.href.includes("weekly")) {
+      pages[0].style.zIndex = "2";
+      pages[1].style.zIndex = "3";
+      pages[2].style.zIndex = "1";
+    }
+    if (e.target.href.includes("monthly")) {
+      pages[0].style.zIndex = "1";
+      pages[1].style.zIndex = "2";
+      pages[2].style.zIndex = "3";
+    }
+  }
+});
